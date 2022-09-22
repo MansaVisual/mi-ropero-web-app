95846f057b7d5422423885d5afaa14b8f736b137
TimoteoGambella <timi.gambella@hotmail.com>
2022-09-22 20:19:02 -0300

    setDescuento

--- src/views/Cart/Cart.jsx
+++ src/views/Cart/Cart.jsx
@@ -57,6 +57,7 @@ const Cart = () => {
     const navigate = useNavigate();
 
     const [descuento,setDescuento]=useState(true)
+    setDescuento(true)
     const location = useLocation();
     const pathnames = location.pathname.split("/").filter((x) => x)
     const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
