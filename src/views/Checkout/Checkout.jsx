57001728b653924926ffcc89a127485b5cf699d7
TimoteoGambella <timi.gambella@hotmail.com>
2022-09-27 16:18:33 -0300

    type view

--- src/views/Checkout/Checkout.jsx
+++ src/views/Checkout/Checkout.jsx
@@ -18,7 +18,7 @@ const Checkout = ()=>{
     const location = useLocation();
     const pathnames = location.pathname.split("/").filter((x) => x)
     
-    const [typeNav,setTypeNav]=useState("info")
+    const [typeNav,setTypeNav]=useState("tarjeta")
 
     const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
 
