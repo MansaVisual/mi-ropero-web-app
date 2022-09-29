79705df8ff69b59a37215c2921bdc433741b10cb
TimoteoGambella <timi.gambella@hotmail.com>
2022-09-29 19:28:58 -0300

    estructuraPopUp

--- src/components/FormCheckout/PopUpME.jsx
+++ src/components/FormCheckout/PopUpME.jsx
@@ -3,27 +3,20 @@ import CancelIcon from '@mui/icons-material/Cancel';
 import shop from "../../assets/img/shop.png"
 import oca from "../../assets/img/OCA.png"
 
-const PopUpSucursales = ({setSucursales,sucursaless})=>{
+const PopUpSucursales = ({setViewSucursales,sucursales})=>{
     return(
         <div className="verSucursales">
-            <div className="fondoPopUp" onClick={()=>setSucursales(false)}></div>
+            <div className="fondoPopUp" onClick={()=>setViewSucursales(false)}></div>
             <div className="popUp">
-                <CancelIcon color="tertiary" className="cross" onClick={()=>setSucursales(false)}/>
+                <CancelIcon color="tertiary" className="cross" onClick={()=>setViewSucursales(false)}/>
                 <img src={shop} alt="SHOP" color="primary" className="botonLogo"/>
                 <img src={oca} alt="OCA" />
                 <p>Sucursales más próximas al domicilio elegido.</p>
                 <div className="cardContainer">
-                    {sucursaless.map(sucursal=>{
+                    {sucursales.map((sucursal,i)=>{
                         return(
-                            <div className="cardSucursal">
-                                {sucursal}
-                            </div>
-                        )
-                    })}
-                    {sucursaless.map(sucursal=>{
-                        return(
-                            <div className="cardSucursal">
-                                {sucursal}
+                            <div className="cardSucursal" key={sucursal+i}>
+                                {sucursal.Calle} {sucursal.Numero}, {sucursal.Localidad}
                             </div>
                         )
                     })}
--- src/components/FormCheckout/PopUpME2.jsx
+++ src/components/FormCheckout/PopUpME2.jsx
@@ -4,34 +4,38 @@ import shop from "../../assets/img/shop.png"
 import { Button, Radio } from "@mui/material";
 import oca from "../../assets/img/OCA.png"
 
-const PopUpSetSucursal = ({setSetSucursal,onClickCheckSucursal,defaultCheckSucursal,handleClickSetSucusarl,sucursalEntrega,sucursaless})=>{
+const PopUpSetSucursal = ({onClickCheckSucursal,defaultCheckSucursal,handleClickSetSucusarl,sucursalEntrega,sucursales})=>{
     return(
-        <div className="setSucursales">
-            <div className="fondoPopUp" onClick={()=>{onClickCheckSucursal("")}}></div>
-            <div className="popUp">
-                <CancelIcon color="tertiary" className="cross" onClick={()=>{onClickCheckSucursal("")}}/>
-                <img src={shop} alt="SHOP" color="primary" className="botonLogo"/>
-                <img src={oca} alt="OCA" />
-                <p>Sucursales más próximas al domicilio elegido.</p>
-                <div className="cardContainer">
-                    {sucursaless.map((sucursal,i)=>{
-                        return(
-                            <div className={`cardSucursal ${sucursalEntrega===sucursal+i && "selected"}`} onClick={()=>onClickCheckSucursal(sucursal+i)}>
-                                <Radio
-                                    name="sucursal"
-                                    id={sucursal+i}
-                                    checked={defaultCheckSucursal(sucursal+i)}
-                                />
-                                {sucursal}
-                            </div>
-                        )
-                    })}
+        <>
+            {sucursales!==undefined &&
+                <div className="setSucursales">
+                    <div className="fondoPopUp" onClick={()=>{onClickCheckSucursal("")}}></div>
+                    <div className="popUp">
+                        <CancelIcon color="tertiary" className="cross" onClick={()=>{onClickCheckSucursal("")}}/>
+                        <img src={shop} alt="SHOP" color="primary" className="botonLogo"/>
+                        <img src={oca} alt="OCA" />
+                        <p>Sucursales más próximas al domicilio elegido.</p>
+                        <div className="cardContainer">
+                            {sucursales.map((sucursal,i)=>{
+                                return(
+                                    <div className={`cardSucursal ${sucursalEntrega===sucursal+i && "selected"}`} onClick={()=>onClickCheckSucursal(sucursal+i)}>
+                                        <Radio
+                                            name="sucursal"
+                                            id={sucursal+i}
+                                            checked={defaultCheckSucursal(sucursal+i)}
+                                            />
+                                        {sucursal.Calle} {sucursal.Numero}, {sucursal.Localidad}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <Button className="botonContinuar" onClick={()=>handleClickSetSucusarl()}>
+                            CONTINUAR
+                        </Button>
+                    </div>
                 </div>
-                <Button className="botonContinuar" onClick={()=>handleClickSetSucusarl()}>
-                    CONTINUAR
-                </Button>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 
