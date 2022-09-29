edfea0b1962ca72d0345df06be3732c6d519886e
TimoteoGambella <timi.gambella@hotmail.com>
2022-09-29 10:24:20 -0300

    datos form

--- src/components/FormCheckout/funciones.jsx
+++ src/components/FormCheckout/funciones.jsx
@@ -1,3 +1,20 @@
+export const chargeForm = async(form,setProvincia)=>{
+    document.getElementById("nombreApellido").value=form.nombreApellido
+    document.getElementById("telefono").value=form.telefono
+    document.getElementById("calle").value=form.calle
+    document.getElementById("alturaKM").value=form.alturaKM
+    document.getElementById("piso").value=form.piso
+    document.getElementById("depto").value=form.depto
+    document.getElementById("barrioLocalidad").value=form.barrioLocalidad
+    document.getElementById("entrecalle1").value=form.entrecalle1
+    document.getElementById("entrecalle2").value=form.entrecalle2
+    document.getElementById("codigoPostal").value=form.codigoPostal
+    document.getElementById("comentario").value=form.comentario
+    if(form.provincia !== ""){
+        setProvincia(form.provincia)
+    }
+}
+
 export const handleChangeForm = async(setForm,form) => {
     await setForm({...form,
         nombreApellido:document.getElementById("nombreApellido").value,
