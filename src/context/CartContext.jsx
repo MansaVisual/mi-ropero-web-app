97515377e0d5b14facc2161d0901fb75db15cd58
TimoteoGambella <timi.gambella@hotmail.com>
2022-10-05 12:11:21 -0300

    insert prdos

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,6 +5,8 @@ export const UseCartContext = createContext();
 export const CartContext = ({children}) => {
     const [carrito,setCarrito]=useState([])
     const [buscandoCart,setBuscandoCart]=useState(true)
+    const [costoCarrito,setCostoCarrito]=useState(false)
+    
 
     const CartAPI = async(data,clase,metodo) =>{
         let resFinal = ''
@@ -30,13 +32,14 @@ export const CartContext = ({children}) => {
         const CartID = new FormData()
 
         CartID.append('idcliente', 68)
-        // CartID.append('idproducto',10610)
+        // CartID.append('idproducto',10277)
         // CartID.append('cantidad',1)
         CartAPI(
             CartID,
             "carritos",
             "all"
         ).then((res)=>{
+            console.log(res)
             if(res.status==="success"){
                 setCarrito(res.result)
                 setBuscandoCart(false)
@@ -46,8 +49,18 @@ export const CartContext = ({children}) => {
         })
     }, []);// eslint-disable-line react-hooks/exhaustive-deps
 
+    useEffect(()=>{
+        let suma = 0
+        if(carrito.length!==0){
+            for(let i=0;i<carrito.length;i++){
+                suma = suma + Number(carrito[i].producto.precio)
+            }
+        }
+        setCostoCarrito(suma)
+    },[carrito])// eslint-disable-line react-hooks/exhaustive-deps
+
     return(
-        <UseCartContext.Provider value={{CartAPI,setCarrito,carrito,buscandoCart,setBuscandoCart}}>
+        <UseCartContext.Provider value={{CartAPI,setCarrito,carrito,buscandoCart,setBuscandoCart,costoCarrito}}>
             {children}
         </UseCartContext.Provider>
     )
