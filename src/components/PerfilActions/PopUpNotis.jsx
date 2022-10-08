23d57db5e71f051da22a75f237532e23ea8e7cd3
TimoteoGambella <timi.gambella@hotmail.com>
2022-10-08 13:58:36 -0300

    popUpNotis

---
+++ src/components/PerfilActions/PopUpNotis.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button } from '@mui/material';
+import cruz from "../../assets/img/cruz.png";
+import MRlogoModal from '../../assets/img/isologo.png'
+
+const PopUpNotis = () => {
+
+  return (
+    <div className="PopUpNotis">
+        <div className="fondoPopUp" 
+            // onClick={()=>setRestorePassword(false)}
+        ></div>
+            <div className="popUp">
+                <div className="popUpContainer">
+                    <img src={MRlogoModal} alt="logo" className="logoModal"/>
+                    <p className="popUpTitle">¿Seguro querés dejar de recibir notificaciones?</p>
+                    <p className="popUpDescription">
+                    Las notificaciones se usan para avisarte si hacés una venta, recibís mensajes, ofertas, etc.
+                    Si las desactivás no recibirás toda esta información.
+                    </p>
+                    <div className='buttonContainer'>
+                        <Button className="volver" >CANCELAR</Button>
+                        <Button className="recordar">DEJAR DE RECIBIR</Button>
+                    </div>
+                    <img
+                        // onClick={() => setRestorePassword(false)}
+                        src={cruz} 
+                        alt="CRUZ" className="cruz" 
+                    />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PopUpNotis
