f521e1b5f1f812a9369fbb38cfc5133599876b2b
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-10-12 21:13:26 -0300

    se conectó popups a mi perfil

--- src/components/PerfilActions/PopUpEliminar.jsx
+++ src/components/PerfilActions/PopUpEliminar.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
+import React from 'react';
 import { Button } from '@mui/material';
-import cruz from "../../assets/img/cruz.png";
-import MRlogoModal from '../../assets/img/isologo.png'
+import cruz from '../../assets/img/cruz.png';
+import MRlogoModal from '../../assets/img/isologo.png';
 
-const PopUpEliminar = () => {
-
+const PopUpEliminar = ({ setDeleteAccount }) => {
   return (
-    <div className="PopUpPerfil">
-        <div className="fondoPopUp" 
-            // onClick={()=>setRestorePassword(false)}
-        ></div>
-            <div className="popUp">
-                <div className="popUpContainer">
-                    <img src={MRlogoModal} alt="logo" className="logoModal"/>
-                    <p className="popUpTitle">¿Seguro querés eliminar tu cuenta?</p>
-                    <p className="popUpDescription">
-                        Se perderán todos los datos y no podrán ser recuperados luego.
-                    </p>
-                    <div className='buttonContainer'>
-                        <Button className="volver" >CANCELAR</Button>
-                        <Button className="recordar">DEJAR DE RECIBIR</Button>
-                    </div>
-                    <img
-                        // onClick={() => setRestorePassword(false)}
-                        src={cruz} 
-                        alt="CRUZ" className="cruz" 
-                    />
-            </div>
+    <div className='PopUpPerfil'>
+      <div className='fondoPopUp' onClick={() => setDeleteAccount(false)}></div>
+      <div className='popUp'>
+        <div className='popUpContainer'>
+          <img src={MRlogoModal} alt='logo' className='logoModal' />
+          <p className='popUpTitle'>¿Seguro querés eliminar tu cuenta?</p>
+          <p className='popUpDescription'>
+            Se perderán todos los datos y no podrán ser recuperados luego.
+          </p>
+          <div className='buttonContainer'>
+            <Button onClick={() => setDeleteAccount(false)} className='volver'>
+              CANCELAR
+            </Button>
+            <Button className='recordar'>ELIMINAR CUENTA</Button>
+          </div>
+          <img
+            onClick={() => setDeleteAccount(false)}
+            src={cruz}
+            alt='CRUZ'
+            className='cruz'
+          />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PopUpEliminar
+export default PopUpEliminar;
--- src/components/PerfilActions/PopUpSesion.jsx
+++ src/components/PerfilActions/PopUpSesion.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
+import React from 'react';
 import { Button } from '@mui/material';
-import cruz from "../../assets/img/cruz.png";
-import MRlogoModal from '../../assets/img/isologo.png'
+import cruz from '../../assets/img/cruz.png';
+import MRlogoModal from '../../assets/img/isologo.png';
 
-const PopUpSesion = () => {
-
+const PopUpSesion = ({ setCloseSession }) => {
   return (
-    <div className="PopUpPerfil">
-        <div className="fondoPopUp" 
-            // onClick={()=>setRestorePassword(false)}
-        ></div>
-            <div className="popUp">
-                <div className="popUpContainer">
-                    <img src={MRlogoModal} alt="logo" className="logoModal"/>
-                    <p className="popUpTitle">¿Seguro querés cerrar sesión?</p>
-                    <p className="popUpDescription">
-                        Luego podrás ingresar con tu cuenta de Mi Ropero, Google o Facebook.
-                    </p>
-                    <div className='buttonContainer'>
-                        <Button className="volver" >CANCELAR</Button>
-                        <Button className="recordar">CERRAR SESIÓN</Button>
-                    </div>
-                    <img
-                        // onClick={() => setRestorePassword(false)}
-                        src={cruz} 
-                        alt="CRUZ" className="cruz" 
-                    />
-            </div>
+    <div className='PopUpPerfil'>
+      <div className='fondoPopUp' onClick={() => setCloseSession(false)}></div>
+      <div className='popUp'>
+        <div className='popUpContainer'>
+          <img src={MRlogoModal} alt='logo' className='logoModal' />
+          <p className='popUpTitle'>¿Seguro querés cerrar sesión?</p>
+          <p className='popUpDescription'>
+            Luego podrás ingresar con tu cuenta de Mi Ropero, Google o Facebook.
+          </p>
+          <div className='buttonContainer'>
+            <Button onClick={() => setCloseSession(false)} className='volver'>
+              CANCELAR
+            </Button>
+            <Button className='recordar'>CERRAR SESIÓN</Button>
+          </div>
+          <img
+            onClick={() => setCloseSession(false)}
+            src={cruz}
+            alt='CRUZ'
+            className='cruz'
+          />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PopUpSesion
+export default PopUpSesion;
