d32788196cb3930836d9f0bb31395ab1e1133dcd
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-10-19 12:08:29 -0300

    prueba deploy

---
+++ src/components/FacebookButton.jsx/FacebookButton.jsx
@@ -0,0 +1,34 @@
+import { Button } from '@mui/material';
+import React from 'react';
+/* import FacebookLogin from 'react-facebook-login'; */
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
+const responseFacebook = (response) => {
+  console.log(response);
+};
+
+const FacebookButton = () => {
+  return (
+    <FacebookLogin
+      appId='425033093123995'
+      autoLoad={false}
+      fields='name,email,picture'
+      callback={responseFacebook}
+      cssClass='botonFB'
+      textButton='INGRESAR CON FACEBOOK'
+      render={(renderProps) => (
+        <Button
+          className='botonFB'
+          onClick={renderProps.onClick}
+          endIcon={<FacebookIcon />}
+        >
+          INGRESAR CON FACEBOOK
+        </Button>
+      )}
+      /* icon='fa-facebook' */
+    />
+  );
+};
+
+export default FacebookButton;
