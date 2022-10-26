9012be7fa07ce0b2ef8d6d23956aa7f720db3361
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-10-26 19:05:03 -0300

    prueba fb

---
+++ src/components/AppleButton/AppleButton.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import { LoginSocialApple } from "reactjs-social-login";
+import { Button } from "reactstrap";
+import { UseLoginContext } from "../../context/LoginContext";
+
+const AppleButton = () => {
+  /*  const { FacebookLogin } = useContext(UseLoginContext); */
+
+  const [data, setData] = useState(null);
+
+  return (
+    <div>
+      <LoginSocialApple
+        client_id={"1515990319"}
+        scope={"name email"}
+        /* redirect_uri={REDIRECT_URI}
+        onLoginStart={onLoginStart} */
+        onResolve={({ provider, data }) => {
+          setData([provider, data]);
+        }}
+        onReject={(err) => {
+          console.log(err);
+        }}
+      >
+        <Button className="botonApple">
+          INGRESAR CON APLEE <i className="fbIcon"></i>
+        </Button>
+      </LoginSocialApple>
+    </div>
+  );
+};
+
+export default AppleButton;
