96a5236e68739d8ccb5f44ea374d318e9fbc6618
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-10-27 11:05:33 -0300

    prueba apple

--- src/components/AppleButton/AppleButton.jsx
+++ src/components/AppleButton/AppleButton.jsx
@@ -21,7 +21,7 @@ const AppleButton = () => {
           setData([provider, data]);
         }}
         onReject={(err) => {
-          /*  console.log(err); */
+          console.log(err);
         }}
       >
         <Button className="botonApple">
