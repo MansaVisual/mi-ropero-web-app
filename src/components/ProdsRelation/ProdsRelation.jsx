34ff5e140b3ceaf63207888e2411fbacc366046b
TimoteoGambella <timi.gambella@hotmail.com>
2022-11-01 15:01:10 -0300

    prodRelation

--- src/components/ProdsRelation/ProdsRelation.jsx
+++ src/components/ProdsRelation/ProdsRelation.jsx
@@ -4,9 +4,11 @@ import { UseColeccionContext } from "../../context/ColeccionesContext";
 import theme from "../../styles/theme";
 import Chip from "../Chip/Chip";
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProdsRelation = () => {
   const { coleccionRecomendados } = useContext(UseColeccionContext);
+  const navigate = useNavigate()
 
   return (
     <Grid item xs={12} sm={12} md={12}>
@@ -23,7 +25,12 @@ const ProdsRelation = () => {
           sx={{
             color: theme.palette.tertiary.main,
             fontSize: theme.typography.fontSize[4],
+            cursor: "pointer",
+            "&:hover":{
+              fontWeight:"700"
+            }
           }}
+          onClick={() => navigate("/colecciones/Recomendados")}
         >
           VER TODOS LOS PRODUCTOS RELACIONADOS
         </Link>
