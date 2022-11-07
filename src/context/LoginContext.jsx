5369b164c3beb63d68f75578a8330d8a3a248df2
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-07 18:07:14 -0300

    prueba apple

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -138,14 +138,11 @@ export const LoginContext = ({ children }) => {
   };
 
   const AppleLogin = (data) => {
-    console.log(data);
     const decoded = jwt_decode(data.authorization.id_token);
-
-    console.log(decoded);
 
     const log = new FormData();
     log.append("social_login_type", 3);
-    log.append("social_login_id", data.authorization.id_token);
+    log.append("social_login_id", decoded.sub);
     LoginAPI(log, "clientes", "login_social").then((res) => {
       console.log(res, Object.fromEntries(log));
 
@@ -163,10 +160,11 @@ export const LoginContext = ({ children }) => {
   };
 
   const AppleRegister = (data) => {
-    console.log(data);
+    const decoded = jwt_decode(data.authorization.id_token);
+
     const log = new FormData();
     log.append("social_login_type", 3);
-    log.append("social_login_id", data.authorization.id_token);
+    log.append("social_login_id", decoded.sub);
     log.append("nombre", data.user.name.firstName);
     log.append("email", data.user.email);
     log.append("apellido", data.user.name.lastName);
