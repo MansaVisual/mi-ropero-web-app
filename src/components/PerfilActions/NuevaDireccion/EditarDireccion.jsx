be9fb6c81d07be8a0c910dfec7cca041c3e0f500
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-09 14:27:19 -0300

    .

--- src/components/PerfilActions/NuevaDireccion/EditarDireccion.jsx
+++ src/components/PerfilActions/NuevaDireccion/EditarDireccion.jsx
@@ -214,6 +214,7 @@ const EditarDireccion = () => {
         res.result[0].calle !== "" &&
         res.result[0].numero !== ""
       ) {
+        console.log(res.result[0].calle, res.result[0].numero);
         scrollTop();
         await setResDirecciones(res.result);
         setViewDireccion(true);
