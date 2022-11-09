238b6568067d14769b59d335f482566f8eb7da3c
TimoteoGambella <timi.gambella@hotmail.com>
2022-11-09 18:16:23 -0300

    productosRelacionados

--- src/views/Cart/Cart.jsx
+++ src/views/Cart/Cart.jsx
@@ -205,7 +205,7 @@ const Cart = () => {
                     </Grid>
                 }
 
-                <ProdsRelation/>
+                {/* <ProdsRelation/> */}
                 
             </Grid>
             {eliminar &&
