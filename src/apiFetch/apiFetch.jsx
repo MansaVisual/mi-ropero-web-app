79a9f67180a63e05520822e12ab9947d36ffee31
TimoteoGambella <timi.gambella@hotmail.com>
2022-11-17 20:49:23 -0300

    error

--- src/apiFetch/apiFetch.jsx
+++ src/apiFetch/apiFetch.jsx
@@ -15,7 +15,7 @@ export const apiFetch=async(data, clase, metodo)=>{
           console.log(data)
           if (data.result === "El producto no existe") {
             Swal.fire({
-              title: `${data.result}`,
+              title: 'EL PRODUCTO NO EXISTE',
               confirmButtonText: "CONTINUAR",
             }).then((res) => {
               window.location.replace("https://www.miropero.ar/");
