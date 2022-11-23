f7f3564c061e46cbf81509e8ac6e5e5c555b9f1a
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-23 16:17:39 -0300

    tienda css

--- src/components/TiendaActions/Mensajes.jsx
+++ src/components/TiendaActions/Mensajes.jsx
@@ -79,90 +79,130 @@ const Mensajes = () => {
       <TiendaBanner />
       <Grid className="tiendaGrid">
         <div className="container">
-          <div className="firstLine">
-            <p className="title">MIS MENSAJES</p>
-            <div className="inputBox">
-              <Select
-                displayEmpty
-                color="primary"
-                className="selectInput"
-                size="small"
-                onChange={(e) => setCurrentType(e.target.value)}
-                value={currentType}
-                renderValue={(selected) => {
-                  if (selected === "") {
-                    return <em>Seleccioná una opción</em>;
-                  }
-                  return selected;
-                }}
-                sx={{
-                  "& div": {
-                    fontSize: "14px",
-                    fontWeight: "400",
-                  },
-                  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                    borderWidth: "1px",
-                  },
-                  height: 42,
-                }}
-              >
-                <MenuItem
-                  disabled
-                  value=""
-                  className="selectOption"
-                  sx={{ fontSize: "14px", color: "#BABCBE", fontWeight: "400" }}
+          <div className="mensajesSection">
+            <div className="firstLine">
+              <p className="title">MIS MENSAJES</p>
+              <div className="inputBox">
+                <Select
+                  displayEmpty
+                  color="primary"
+                  className="selectInput"
+                  size="small"
+                  onChange={(e) => setCurrentType(e.target.value)}
+                  value={currentType}
+                  renderValue={(selected) => {
+                    if (selected === "") {
+                      return <em>Seleccioná una opción</em>;
+                    }
+                    return selected;
+                  }}
+                  sx={{
+                    "& div": {
+                      fontSize: "14px",
+                      fontWeight: "400",
+                    },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderWidth: "1px",
+                    },
+                    height: 42,
+                  }}
                 >
-                  <em>Seleccioná </em>
-                </MenuItem>
-                {typeMessages.map((option) => (
                   <MenuItem
-                    key={option}
-                    value={option}
-                    sx={{ fontSize: "14px", color: "#969696" }}
+                    disabled
+                    value=""
                     className="selectOption"
+                    sx={{
+                      fontSize: "14px",
+                      color: "#BABCBE",
+                      fontWeight: "400",
+                    }}
                   >
-                    {option}
+                    <em>Seleccioná </em>
                   </MenuItem>
-                ))}
-              </Select>
-            </div>
-          </div>
-          <div className="mensajesContainer">
-            {!mensajesFinBusqueda ? (
-              <div
-                style={{
-                  height: "50vh",
-                  marginTop: "42px",
-                  width: "100%",
-                  maxWidth: "895px",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <Loader spin={"spinnerM"} />
-              </div>
-            ) : mensajesFiltrados.length > 0 ? (
-              mensajesFiltrados.map((mensaje, id) => {
-                return (
-                  <>
-                    <div
-                      key={id}
-                      className="desktopCard"
-                      onClick={() => {
-                        setMensajeId(mensaje.idmensaje);
-                        navigate(`/MiTienda/MI CHAT`);
-                      }}
+                  {typeMessages.map((option) => (
+                    <MenuItem
+                      key={option}
+                      value={option}
+                      sx={{ fontSize: "14px", color: "#969696" }}
+                      className="selectOption"
                     >
-                      <div className="mensajeData">
+                      {option}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </div>
+            </div>
+            <div className="mensajesContainer">
+              {!mensajesFinBusqueda ? (
+                <div
+                  style={{
+                    height: "50vh",
+                    marginTop: "42px",
+                    width: "100%",
+                    maxWidth: "895px",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Loader spin={"spinnerM"} />
+                </div>
+              ) : mensajesFiltrados.length > 0 ? (
+                mensajesFiltrados.map((mensaje, id) => {
+                  return (
+                    <>
+                      <div
+                        key={id}
+                        className="desktopCard"
+                        onClick={() => {
+                          setMensajeId(mensaje.idmensaje);
+                          navigate(`/MiTienda/MI CHAT`);
+                        }}
+                      >
+                        <div className="mensajeData">
+                          <img
+                            src={mensaje.producto.imagenes[0].imagen_cuadrada}
+                            alt="cardImage"
+                            onError={(e) => handleAvatarError(e)}
+                          />
+                          <div>
+                            <p className="messageTitle">
+                              {mensaje.producto.nombre}
+                            </p>
+                            <p className="messageDate">
+                              {formatoFecha(mensaje.fecha)}
+                            </p>
+                            <p className="messageMessage">{mensaje.mensaje}</p>
+                            <p className="messageState">
+                              {mensajesEstado[Number(mensaje.estado)]}
+                            </p>
+                          </div>
+                        </div>
+                        <img
+                          onClick={() => {
+                            setBorrarMsj(true);
+                            setMensajeId(mensaje.idmensaje);
+                          }}
+                          className="basuraIcon"
+                          src={basura}
+                          alt="BasuraIcon"
+                        />
+                      </div>
+                      <div
+                        key={`mobile${id}`}
+                        className="mobileCard"
+                        onClick={() => {
+                          setMensajeId(mensaje.idmensaje);
+                          navigate(`/MiTienda/MI CHAT`);
+                        }}
+                      >
                         <img
                           src={mensaje.producto.imagenes[0].imagen_cuadrada}
+                          className="productImg"
                           alt="cardImage"
                           onError={(e) => handleAvatarError(e)}
                         />
                         <div>
-                          <p className="messageTitle">
-                            {mensaje.producto.nombre}
-                          </p>
+                          <p className="messageTitle">{mensaje.titulo}</p>
                           <p className="messageDate">
                             {formatoFecha(mensaje.fecha)}
                           </p>
@@ -171,62 +211,28 @@ const Mensajes = () => {
                             {mensajesEstado[Number(mensaje.estado)]}
                           </p>
                         </div>
+                        <img
+                          src={basura}
+                          className="trashICon"
+                          alt="basuraIcon"
+                        />
                       </div>
-                      <img
-                        onClick={() => {
-                          setBorrarMsj(true);
-                          setMensajeId(mensaje.idmensaje);
-                        }}
-                        className="basuraIcon"
-                        src={basura}
-                        alt="BasuraIcon"
-                      />
-                    </div>
-                    <div
-                      key={`mobile${id}`}
-                      className="mobileCard"
-                      onClick={() => {
-                        setMensajeId(mensaje.idmensaje);
-                        navigate(`/MiTienda/MI CHAT`);
-                      }}
-                    >
-                      <img
-                        src={mensaje.producto.imagenes[0].imagen_cuadrada}
-                        className="productImg"
-                        alt="cardImage"
-                        onError={(e) => handleAvatarError(e)}
-                      />
-                      <div>
-                        <p className="messageTitle">{mensaje.titulo}</p>
-                        <p className="messageDate">
-                          {formatoFecha(mensaje.fecha)}
-                        </p>
-                        <p className="messageMessage">{mensaje.mensaje}</p>
-                        <p className="messageState">
-                          {mensajesEstado[Number(mensaje.estado)]}
-                        </p>
-                      </div>
-                      <img
-                        src={basura}
-                        className="trashICon"
-                        alt="basuraIcon"
-                      />
-                    </div>
-                  </>
-                );
-              })
-            ) : (
-              <div className="perfilVacio">
-                <div>
-                  <img src={mensaje} alt="LOGO" />
-                  <p>Aún no tienes mensajes "{currentType}"</p>
-                  <Button onClick={() => navigate(`/`)}>IR A INICIO</Button>
+                    </>
+                  );
+                })
+              ) : (
+                <div className="perfilVacio">
+                  <div>
+                    <img src={mensaje} alt="LOGO" />
+                    <p>Aún no tienes mensajes "{currentType}"</p>
+                    <Button onClick={() => navigate(`/`)}>IR A INICIO</Button>
+                  </div>
                 </div>
+              )}
+              <div className="returnLink" onClick={() => navigate(`/MiTienda`)}>
+                <img src={leftArrow} alt="leftArrow" />
+                <p>VOLVER A MI TIENDA</p>
               </div>
-            )}
-            <div className="returnLink" onClick={() => navigate(`/MiTienda`)}>
-              <img src={leftArrow} alt="leftArrow" />
-              <p>VOLVER A MI TIENDA</p>
             </div>
           </div>
         </div>
