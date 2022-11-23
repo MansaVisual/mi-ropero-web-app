e5accdbef918e31e0017f423682db1fa9775b53d
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-22 22:03:38 -0300

    mensajes

--- src/components/PerfilActions/MisMensajes.jsx
+++ src/components/PerfilActions/MisMensajes.jsx
@@ -47,7 +47,7 @@ const MisMensajes = () => {
 
   useEffect(() => {
     if (mensajes.length > 0) {
-      const lista = mensajes.filter((msg) => msg.idmensajepadre === "0");
+      const lista = mensajes.filter((msg) => msg.idmensaje === "0");
       console.log(lista);
       if (typeMessage === "ver todos") {
         setMensajesFiltrados(lista);
