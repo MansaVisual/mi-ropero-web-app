e23fd6dd2dbbeb3b21c11653bcd9ba86c1221681
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-23 17:20:27 -0300

    tienda css

--- src/components/TiendaActions/Calificaciones.jsx
+++ src/components/TiendaActions/Calificaciones.jsx
@@ -172,51 +172,53 @@ const Calificaciones = () => {
             </div>
           </div>
           <div className="bottomContainer">
-            <div className="returnLink" onClick={() => navigate(`/MiTienda`)}>
-              <img src={leftArrow} alt="leftArrow" />
-              <p>VOLVER A MI TIENDA</p>
-            </div>
-            <Select
-              displayEmpty
-              className="selectInput"
-              onChange={(e) => setSelected(e.target.value)}
-              value={selected}
-              renderValue={(selected) => {
-                if (selected === "") {
-                  return <em>Seleccioná una opción</em>;
-                }
-                return selected;
-              }}
-              sx={{
-                "& div": {
-                  fontSize: "14px",
-                  fontWeight: "400",
-                },
-                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  borderWidth: "1px",
-                },
-                height: 42,
-              }}
-            >
-              <MenuItem
-                disabled
-                value=""
-                className="selectOption"
-                sx={{ fontSize: "14px", color: "#BABCBE", fontWeight: "400" }}
+            <div className="bottomSection">
+              <div className="returnLink" onClick={() => navigate(`/MiTienda`)}>
+                <img src={leftArrow} alt="leftArrow" />
+                <p>VOLVER A MI TIENDA</p>
+              </div>
+              <Select
+                displayEmpty
+                className="selectInput"
+                onChange={(e) => setSelected(e.target.value)}
+                value={selected}
+                renderValue={(selected) => {
+                  if (selected === "") {
+                    return <em>Seleccioná una opción</em>;
+                  }
+                  return selected;
+                }}
+                sx={{
+                  "& div": {
+                    fontSize: "14px",
+                    fontWeight: "400",
+                  },
+                  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    borderWidth: "1px",
+                  },
+                  height: 42,
+                }}
               >
-                <em>Seleccioná </em>
-              </MenuItem>
-              {stateList.map((option) => (
                 <MenuItem
-                  key={option}
-                  value={option}
-                  sx={{ fontSize: "14px", color: "#969696" }}
+                  disabled
+                  value=""
                   className="selectOption"
+                  sx={{ fontSize: "14px", color: "#BABCBE", fontWeight: "400" }}
                 >
-                  {option}
+                  <em>Seleccioná </em>
                 </MenuItem>
-              ))}
-            </Select>
+                {stateList.map((option) => (
+                  <MenuItem
+                    key={option}
+                    value={option}
+                    sx={{ fontSize: "14px", color: "#969696" }}
+                    className="selectOption"
+                  >
+                    {option}
+                  </MenuItem>
+                ))}
+              </Select>
+            </div>
           </div>
         </div>
       </Grid>
