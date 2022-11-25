3e6e185e7c1d5817c5d48ea3a1cc5a7fccc686c6
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-25 15:44:17 -0300

    img

--- src/components/TiendaActions/PopUpImg.jsx
+++ src/components/TiendaActions/PopUpImg.jsx
@@ -113,6 +113,24 @@ const PopUpImg = ({
     let newImage = new Image();
     newImage.src = canvasData;
 
+    function dataURLtoFile(dataurl, filename) {
+      var arr = dataurl.split(","),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+
+      return new File([u8arr], filename, { type: mime });
+    }
+
+    //Usage example:
+    var file = dataURLtoFile(newImage, "hello.png");
+    console.log(file);
+
     return newImage;
 
     /* return new Promise((resolve, reject) => {
