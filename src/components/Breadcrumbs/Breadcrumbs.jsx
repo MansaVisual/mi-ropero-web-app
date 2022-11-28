ea6216d23a51769c47c634f7f631567fe6b46287
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-28 16:48:27 -0300

    tienda breadcrumb fix

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -57,7 +57,7 @@ const Breadcrumbs = ({ links }) => {
               color: theme.palette.quaternary.contrastText,
             }}
           >
-            {link.replace(/%20/g, " ")}
+            {link === "MiTienda" ? "Mi Tienda" : link.replace(/%20/g, " ")}
           </Link>
         );
       })}
