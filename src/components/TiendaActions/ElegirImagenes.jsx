5d0b28e0856e740c0206f1cd1555a2b323286310
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-30 19:15:38 -0300

    edit img

--- src/components/TiendaActions/ElegirImagenes.jsx
+++ src/components/TiendaActions/ElegirImagenes.jsx
@@ -66,6 +66,7 @@ const ElegirImagenes = ({ form, setForm }) => {
           let img = {};
           setImgNecesarias(categorias[i].imagenes_necesarias);
           const { imagenes } = form.prodEditar;
+          console.log(imagenes);
 
           for (let j = 0; j < categorias[i].imagenes_necesarias.length; j++) {
             let obj = categorias[i].imagenes_necesarias[j].nombre;
