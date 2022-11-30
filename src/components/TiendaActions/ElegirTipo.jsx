2735bb4cea91d447db083ba0c2d00621dc1b362f
Ferrer Gonzalo Facundo <ferrerfacundo96@gmail.com>
2022-11-29 21:53:52 -0300

    editproduct

--- src/components/TiendaActions/ElegirTipo.jsx
+++ src/components/TiendaActions/ElegirTipo.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment,useEffect } from "react";
+import React, { useContext, Fragment, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import leftArrow from "../../assets/img/leftArrow.png";
@@ -11,32 +11,32 @@ const ElegirTipo = ({ form, setForm }) => {
   const pathnames = location.pathname.split("/").filter((x) => x);
   const { categorias } = useContext(UseProdsContext);
 
-  
   useEffect(() => {
     if (!form.categoriaId && !form.prodEditar) {
       navigate(`/Mi&Tienda/CATEGORIA`);
       return;
     }
-    if(form.editarProd!==undefined && form.editarProd){
-      for(const i in categorias){
-        console.log(categorias[i])
-        if(categorias[i].idcategoria===form.prodEditar.idcategoria){
-          setForm((prevState)=>({
+    if (form.editarProd !== undefined && form.editarProd) {
+      for (const i in categorias) {
+        console.log(categorias[i]);
+        if (categorias[i].idcategoria === form.prodEditar.idcategoria) {
+          setForm((prevState) => ({
             ...prevState,
-            editarProd:false,
-            categoriaId:categorias[i].idcategoriapadre,
-            tipoId:categorias[i].idcategoria,
+            editarProd: false,
+            categoriaId: categorias[i].idcategoriapadre,
+            tipoId: categorias[i].idcategoria,
             tipoNombre: categorias[i].nombre,
             caracteristicas: form.prodEditar.caracteristicas,
+            idCaracteristica: form.prodEditar.caracteristicas.split(","),
             titulo: form.prodEditar.nombre,
             precio: form.prodEditar.precio,
             descripcion: form.prodEditar.descripcion,
-          }))
+          }));
         }
       }
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
-  console.log(form)
+  console.log(form);
 
   return (
     <div className="elegirTipoContainer">
@@ -45,46 +45,48 @@ const ElegirTipo = ({ form, setForm }) => {
         <span className="title">ROPA</span>
         <span className="subtitle">¿Cuál es la subcategoría del producto?</span>
         <div className="subTypeSection">
-          {form.categoriaId!==null &&<>
-            {categorias.map((cat, i) => {
-              return (
-                <Fragment key={i}>
-                  {cat.idcategoriapadre === form.categoriaId.toString() && (
-                    <div
-                      className="subType"
-                      onClick={() =>
-                        setForm((prevState) => ({
-                          ...prevState,
-                          tipoId: cat.idcategoria,
-                          tipoNombre: cat.nombre,
-                          caracteristicas: [],
-                          idCaracteristica: [],
-                          idCaracteristicaOld: [],
-                        }))
-                      }
-                    >
-                      <Radio
-                        checked={form.tipoId === cat.idcategoria}
-                        className="radio"
-                        name="radioButton"
-                        id={`radioButton-${i}`}
-                      />
-                      <label
-                        for={`radioButton-${i}`}
-                        className={
-                          form.tipoId === cat.idcategoria
-                            ? "selected"
-                            : "notSelected"
+          {form.categoriaId !== null && (
+            <>
+              {categorias.map((cat, i) => {
+                return (
+                  <Fragment key={i}>
+                    {cat.idcategoriapadre === form.categoriaId.toString() && (
+                      <div
+                        className="subType"
+                        onClick={() =>
+                          setForm((prevState) => ({
+                            ...prevState,
+                            tipoId: cat.idcategoria,
+                            tipoNombre: cat.nombre,
+                            caracteristicas: [],
+                            idCaracteristica: [],
+                            idCaracteristicaOld: [],
+                          }))
                         }
                       >
-                        {cat.nombre}
-                      </label>
-                    </div>
-                  )}
-                </Fragment>
-              );
-            })}</>
-          }
+                        <Radio
+                          checked={form.tipoId === cat.idcategoria}
+                          className="radio"
+                          name="radioButton"
+                          id={`radioButton-${i}`}
+                        />
+                        <label
+                          for={`radioButton-${i}`}
+                          className={
+                            form.tipoId === cat.idcategoria
+                              ? "selected"
+                              : "notSelected"
+                          }
+                        >
+                          {cat.nombre}
+                        </label>
+                      </div>
+                    )}
+                  </Fragment>
+                );
+              })}
+            </>
+          )}
         </div>
         <div className="buttonContainer">
           <button
@@ -99,15 +101,15 @@ const ElegirTipo = ({ form, setForm }) => {
             IR A IMÁGENES
           </button>
         </div>
-        {form.prodEditar===undefined &&
+        {form.prodEditar === undefined && (
           <div
-          className="returnLink"
-          onClick={() => navigate(`/Mi&Tienda/CATEGORIA`)}
+            className="returnLink"
+            onClick={() => navigate(`/Mi&Tienda/CATEGORIA`)}
           >
             <img src={leftArrow} alt="leftArrow" />
             <p>VOLVER A MI CATEGORIA</p>
           </div>
-        }
+        )}
       </div>
     </div>
   );
